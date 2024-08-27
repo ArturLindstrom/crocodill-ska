9bed0b47c3a1368279abf51b79c9935705739c4f
arturlindstrom <artur.lindstrom@gmail.com>
2024-08-27 23:23:20 +0200

    add preschool route and test data

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,10 @@
+import Preschool from "@/routes/preSchool";
 import Root from "@/routes/root";
 import { createBrowserRouter } from "react-router-dom";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [{ path: "/förskolor/:name", element: <Preschool /> }],
   },
 ]);
