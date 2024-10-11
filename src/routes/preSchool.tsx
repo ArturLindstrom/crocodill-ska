5b39a720808ee0aa5534340bf60beea36539103f
arturlindstrom <artur.lindstrom@gmail.com>
2024-10-11 20:18:22 +0200

    Refactor Preschool component to remove unused code and improve readability

--- src/routes/preSchool.tsx
+++ src/routes/preSchool.tsx
@@ -1,27 +1,8 @@
-import Heading from "@/components/typography/headings";
 import PreschoolLayout from "@/layouts/preschoolLayout";
 
-import { useParams } from "react-router-dom";
-
 const Preschool = () => {
-  const { name } = useParams();
-  const nameAggregated = () => {
-    switch (name) {
-      case "ilcrocodill":
-        return "Il Crocodill";
-      case "entillcrocodill":
-        return "Entill Crocodill";
-      case "lillcrocodill":
-        return "Lill Crocodill";
-      case "afcrocodill":
-        return "Af Crocodill";
-      default:
-        return "Unknown";
-    }
-  };
   return (
     <>
-      <Heading.H1>{nameAggregated()}</Heading.H1>
       <PreschoolLayout />
     </>
   );
