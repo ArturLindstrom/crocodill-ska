20ed3be045c10fee42bb89f2b2b18317980b6201
arturlindstrom <artur.lindstrom@gmail.com>
2024-10-11 20:16:50 +0200

    Refactor ComboBox component to implement a searchable dropdown menu with options

---
+++ src/components/comboBox/index.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type ComboBoxProps = {
+  placeholder?: string;
+  options: { value: string; label: string }[];
+  value: string | null; // Controlled value from parent
+  onSelect: (value: string | null) => void; // Callback to notify parent of changes
+};
+
+const Combobox = ({ placeholder, options, value, onSelect }: ComboBoxProps) => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : `Välj ${placeholder}...`}
+          <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder={`Sök efter ${placeholder}...`} />
+          <CommandList>
+            <CommandEmpty>Ingen {placeholder} hittades.</CommandEmpty>
+            <CommandGroup>
+              {options.map((option) => (
+                <CommandItem
+                  key={option.value}
+                  value={option.value}
+                  onSelect={() => {
+                    onSelect(option.value === value ? null : option.value); // Notify parent with selected value
+                    setOpen(false); // Close the dropdown after selection
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === option.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {option.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default Combobox;
