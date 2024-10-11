5aa31034dc482c042fa21a443ccfe935ab32b594
arturlindstrom <artur.lindstrom@gmail.com>
2024-10-11 20:18:15 +0200

    Refactor router to include preschool loader and dynamic preschool routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,26 @@
+import { preschoolLoader } from "@/api/loaders";
+import { getAllPreschools } from "@/api/preschools";
 import Preschool from "@/routes/preSchool";
 import Root from "@/routes/root";
 import { createBrowserRouter } from "react-router-dom";
+
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ path: "/förskolor/:name", element: <Preschool /> }],
+    loader: getAllPreschools,
+    children: [
+      {
+        path: "förskolor/:id",
+        element: <Preschool />,
+        loader: ({ params }) => {
+          const { id } = params;
+          if (!id) {
+            throw new Error("Preschool ID is required.");
+          }
+          return preschoolLoader(id);
+        },
+      },
+    ],
   },
 ]);
