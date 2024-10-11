36caf682aa9fbd98d9dcef24b68477f0bf170730
arturlindstrom <artur.lindstrom@gmail.com>
2024-10-11 20:19:02 +0200

    Add types for database tables and documentation with teachers

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+import { Database } from "./supabase";
+
+export type Criteria = Database["public"]["Tables"]["criteria"]["Row"];
+export type Department = Database["public"]["Tables"]["departments"]["Row"];
+export type Month = Database["public"]["Tables"]["months"]["Row"];
+export type Preschool = Database["public"]["Tables"]["preschools"]["Row"];
+export type Student = Database["public"]["Tables"]["students"]["Row"];
+export type Teacher = Database["public"]["Tables"]["teachers"]["Row"];
+export type Term = Database["public"]["Tables"]["terms"]["Row"];
+export type Documentation =
+  Database["public"]["Tables"]["documentations"]["Row"];
+
+export type PreschoolLoader = {
+  preschool: Preschool;
+  departments: Department[];
+  terms: Term[];
+  months: Month[];
+  documentations: DocumentationWithTeacher[];
+};
+
+// Assuming Documentation is defined and teachers are fetched with each documentation
+export interface DocumentationWithTeacher extends Documentation {
+  teachers: {
+    name: string; // Or other relevant fields you fetch from the teachers table
+  };
+}
