8796784c43747ca3478d0a7add587b1f2b5c1d61
arturlindstrom <artur.lindstrom@gmail.com>
2024-11-19 21:51:24 +0100

    Refactor documentation store to manage state and actions for documentation creation

--- src/store/documentations/index.ts
+++ src/store/documentations/index.ts
@@ -1,14 +1,96 @@
-import { DocumentationWithTeacher } from "@/types";
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { DocumentationActions, SetInstitutionPayload } from "./types";
+import { DocumentationState } from "./types/shared";
+import { updateOrAddArea, removeCriteria } from "./utils/documentationUtils";
 
-interface DocumentationState {
-  documentations: DocumentationWithTeacher[];
-  setDocumentations: (docs: DocumentationWithTeacher[]) => void;
-  resetDocumentations: () => void;
-}
+const initialState: DocumentationState = {
+  documentationName: "",
+  students: [],
+  studentIds: [],
+  selectedAreasWithCriteria: [],
+  institution: {
+    preschoolId: null,
+    preschoolName: null,
+    departmentId: null,
+    departmentName: null,
+  },
+  teacher: {
+    id: null,
+    name: null,
+  },
+  month: {
+    monthId: null,
+    monthName: null,
+  },
+  term: {
+    termId: null,
+    termName: null,
+  },
+};
 
-export const useDocumentationStore = create<DocumentationState>((set) => ({
-  documentations: [],
-  setDocumentations: (docs) => set({ documentations: docs }),
-  resetDocumentations: () => set({ documentations: [] }),
-}));
+export const useDocumentationStore = create<
+  DocumentationState & DocumentationActions
+>()(
+  devtools((set, get) => ({
+    ...initialState,
+
+    // Actions
+    setDocumentationName: (documentationName) =>
+      set({ documentationName }, false, "setDocumentationName"),
+
+    setStudents: (students) => set({ students }, false, "setStudents"),
+
+    setInstitution: (institution: SetInstitutionPayload) =>
+      set({ institution }, false, "setInstitution"),
+
+    setTeacher: (id: number, name: string) =>
+      set({ teacher: { id, name } }, false, "setTeacher"),
+
+    setAreaCriteria: (areaId, areaName, criteria, checked) => {
+      set(
+        (state) => ({
+          selectedAreasWithCriteria: checked
+            ? updateOrAddArea(
+                state.selectedAreasWithCriteria,
+                areaId,
+                areaName,
+                criteria
+              )
+            : removeCriteria(
+                state.selectedAreasWithCriteria,
+                areaId,
+                criteria.criteria_id
+              ),
+        }),
+        false,
+        "setAreaCriteria"
+      );
+    },
+
+    setStudentIds: (studentIds: number[]) =>
+      set({ studentIds }, false, "setStudentIds"),
+
+    getDocumentationData: () => get(),
+
+    setMonth: (payload) =>
+      set(
+        {
+          month: { monthId: payload.monthId, monthName: payload.monthName },
+        },
+        false,
+        "setMonth"
+      ),
+
+    setTerm: (payload) =>
+      set(
+        {
+          term: { termId: payload.termId, termName: payload.termName },
+        },
+        false,
+        "setTerm"
+      ),
+
+    resetForm: () => set(initialState, false, "resetForm"),
+  }))
+);
