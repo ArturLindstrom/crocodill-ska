5d1359427fe54625ef54b196282a3286ea8d70e5
arturlindstrom <artur.lindstrom@gmail.com>
2024-11-28 22:34:29 +0100

    remove statistics from singe preschool page and start adding blocks

--- src/routes/preSchool.tsx
+++ src/routes/preSchool.tsx
@@ -1,63 +1,44 @@
 import { usePreschoolData } from "@/api/queries/preschools/usePreschoolQueries";
-import PieChartCard from "@/components/chart/pieChart";
-import TermChart from "@/components/chart/termChart";
-import PreschoolDataForm from "@/components/forms/preschoolDataForm";
 import Loader from "@/components/loader";
-import DocumentationTable from "@/components/table/documentationTable";
 import Heading from "@/components/typography/headings";
-import { Card } from "@/components/ui/card";
-import { DocumentationWithTeacher } from "@/types";
-import { useState, useEffect, useCallback } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import { useParams } from "react-router-dom";
 
 const Preschool = () => {
-  const [filteredDocumentations, setFilteredDocumentations] = useState<
-    DocumentationWithTeacher[]
-  >([]);
   const { id } = useParams();
 
   const { data, isLoading, error } = usePreschoolData(id);
-
-  useEffect(() => {
-    if (data?.documentations) {
-      setFilteredDocumentations(data.documentations);
-    }
-  }, [data]);
-
-  const handleDocumentationsFetch = useCallback(
-    (docs: DocumentationWithTeacher[]) => {
-      setFilteredDocumentations(docs);
-    },
-    []
-  );
+  console.log("data", data);
 
   if (isLoading) return <Loader />;
   if (error) return <div>Error fetching data: {(error as Error).message}</div>;
   if (!data) return <div>No data found.</div>;
-
-  const { preschool, departments, terms, months, countByMonth } = data;
+  const { name, teachers } = data;
 
   return (
-    <>
-      <div className="flex flex-col items-center gap-4 p-4">
-        <Heading.H1>{preschool?.name || "Unknown Preschool"}</Heading.H1>{" "}
-        <PreschoolDataForm
-          departments={departments || []}
-          terms={terms}
-          months={months || []}
-          onDocumentationsFetch={handleDocumentationsFetch}
-        />
-      </div>
-
-      <Card className="grid w-5/6 grid-cols-2 p-4">
-        <TermChart
-          countByMonth={countByMonth || {}}
-          term={terms[0].term_name || ""}
-        />
-        <PieChartCard />
-        <DocumentationTable documentations={filteredDocumentations} />
-      </Card>
-    </>
+    <Card className="grid w-full grid-cols-2 p-4 grgap-4">
+      <CardHeader className="col-span-2 text-center">
+        <Heading.H1>{name}</Heading.H1>
+      </CardHeader>
+      <CardContent>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Pedagoger</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul>
+              {teachers.map((teacher) => (
+                <li key={teacher.teacher_id}>
+                  {teacher.name}
+                  <Label>{teacher.department.name}</Label>
+                </li>
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+      </CardContent>
+    </Card>
   );
 };
 
